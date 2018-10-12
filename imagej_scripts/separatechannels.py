beb4ccfe0902a5b368cdc86e73b3e3e2a91c3fa7
kateharline <harhark8@gmail.com>
2018-10-12 10:49:49 -0400

    still a little buggy if mismatched number of channels, but not sure why

--- imagej_scripts/separatechannels.py
+++ imagej_scripts/separatechannels.py
@@ -6,16 +6,11 @@ from ij import IJ
 from ij import WindowManager as WM
 
 # change these w each run #
-input_path = '/Users/kateharline/Documents/roeder_lab/imaging/leaf_live_beta/example'
-num_channels = 2
+input_path = '/Users/kateharline/Desktop/outer'
 
 # probably don't change -- unless you are changing code functionality
 start_extension = '.lsm'
 new_extension = '.tif'
-output_path = os.path.join(input_path, 'channels_separated')
-
-if not os.path.exists(output_path):
-	os.makedirs(output_path)
 
 def load(path):
 	IJ.open(path)
@@ -24,18 +19,27 @@ def load(path):
 
 def process(filename, output_path):
 	IJ.run("Split Channels")
+	# the number of channels is one (orig image) less than the number of images opened
+	num_channels = WM.getImageCount() - 1
 	for i in range(0, num_channels):
 		curr_img = WM.getCurrentImage()
 		IJ.saveAs("Tiff", os.path.join(output_path, 'C-'+ str(i+1) + '_' + filename))
 		curr_img.close()
 	return
 	
-def batch_process(extension, source_dir, output_dir):
-	for filename in os.listdir(source_dir):
-		if filename.endswith(extension):
-			img = load(os.path.join(source_dir, filename))
-			process(filename, output_path)
-			img.close()
+def batch_process(extension, source_dir):
+	for folder, subs, files in os.walk(source_dir):
+		print('folder' +str(folder))
+		print('subs' +str(subs))
+		print('files' + str(files))
+        output_path = os.path.join(folder, 'channels_separated')
+        for filename in files:
+            if filename.endswith(extension):
+                if not os.path.exists(output_path):
+                    os.makedirs(output_path)
+                img = load(os.path.join(folder, filename))
+                process(filename, output_path)
+                img.close()
 	return
 
-batch_process(start_extension, input_path, output_path)
+batch_process(start_extension, input_path)
