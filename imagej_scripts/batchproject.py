ca2fde181c226aa624846d0f91d026fe31e09d6a
kateharline <harhark8@gmail.com>
2019-02-21 20:40:27 -0500

    asdf

--- imagej_scripts/batchproject.py
+++ imagej_scripts/batchproject.py
@@ -18,15 +18,20 @@ def load(path):
     return img
 
 def process(filename, output_path):
-    #IJ.run("Z Project...", "projection=[Max Intensity]");
+    IJ.run("Z Project...", "projection=[Max Intensity]");
     # the number of channels is one (orig image) less than the number of images opened
-    curr_img = WM.getCurrentImage()
+    
     # add scale bar to image
     IJ.run("Scale Bar...", "width=50 height=20 font=18 color=White background=None location=[Lower Right] hide overlay");
     # save scale bar to stack
     IJ.run("Flatten");
+    
     IJ.saveAs("Tiff", os.path.join(output_path, 'maxint_w_scale' + filename))
-    curr_img.close()
+    # close all the images 
+    num_channels = WM.getImageCount()
+    for i in range(0, num_channels):
+    	curr_img = WM.getCurrentImage()
+    	curr_img.close()
     return
 
 def batch_process(extension, source_dir):
@@ -38,7 +43,6 @@ def batch_process(extension, source_dir):
                     os.makedirs(output_path)
                 img = load(os.path.join(folder, filename))
                 process(filename, output_path)
-                img.close()
     return
 
 batch_process(start_extension, input_path)
