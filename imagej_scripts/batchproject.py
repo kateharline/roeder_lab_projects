d9383fbc106b5762911dd39458be3308b80ba1b4
kateharline <harhark8@gmail.com>
2019-02-21 19:58:26 -0500

    file naming

--- imagej_scripts/batchproject.py
+++ imagej_scripts/batchproject.py
@@ -6,7 +6,7 @@ from ij import IJ
 from ij import WindowManager as WM
 
 # change these w each run #
-input_path = '/Users/kateharline/Desktop/make_max_int'
+input_path = '/Users/kateharline/Desktop/max_int_w_scale'
 
 # probably don't change -- unless you are changing code functionality
 start_extension = '.tif'
@@ -18,18 +18,18 @@ def load(path):
     return img
 
 def process(filename, output_path):
-    IJ.run("Z Project...", "projection=[Max Intensity]");
+    #IJ.run("Z Project...", "projection=[Max Intensity]");
     # the number of channels is one (orig image) less than the number of images opened
     curr_img = WM.getCurrentImage()
     # add scale bar to image
     IJ.run("Scale Bar...", "width=50 height=20 font=18 color=White background=None location=[Lower Right] hide overlay");
-    IJ.saveAs("Tiff", os.path.join(output_path, 'maxint_' + filename))
+    IJ.saveAs("Tiff", os.path.join(output_path, 'maxint_w_scale' + filename))
     curr_img.close()
     return
 
 def batch_process(extension, source_dir):
     for folder, subs, files in os.walk(source_dir):
-        output_path = os.path.join(folder, 'max_int')
+        output_path = os.path.join(folder, 'max_int_w_scale')
         for filename in files:
             if filename.endswith(extension):
                 if not os.path.exists(output_path):
