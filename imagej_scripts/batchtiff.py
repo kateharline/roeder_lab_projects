313c8da946a500744320f6f65bd899554a96d200
kateharline <harhark8@gmail.com>
2019-04-24 10:37:03 -0400

    add scale in batch tiff

--- imagej_scripts/batchtiff.py
+++ imagej_scripts/batchtiff.py
@@ -20,13 +20,17 @@ def load(path):
 def process(filename, output_path):
     # the number of channels is one (orig image) less than the number of images opened
     curr_img = WM.getCurrentImage()
-    IJ.saveAs("Tiff", os.path.join(output_path, 'maxint_' + filename))
+        # add scale bar to image
+    IJ.run("Scale Bar...", "width=50 height=20 font=18 color=White background=None location=[Lower Right] hide overlay");
+    # save scale bar to stack
+    IJ.run("Flatten");
+    IJ.saveAs("Tiff", os.path.join(output_path, 'w_scale_' + filename))
     curr_img.close()
     return
 
 def batch_process(extension, source_dir):
     for folder, subs, files in os.walk(source_dir):
-        output_path = os.path.join(folder, 'max_int')
+        output_path = os.path.join(folder, 'scale_tiffs')
         for filename in files:
             if filename.endswith(extension):
                 if not os.path.exists(output_path):
