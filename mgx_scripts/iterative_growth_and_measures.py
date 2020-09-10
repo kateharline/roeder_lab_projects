d9ed4ee78254058f968cdc38f35f7f27b10d6659
kateharline <harhark8@gmail.com>
2020-09-10 10:33:43 -0400

    simpler small problem for testing

--- mgx_scripts/iterative_growth_and_measures.py
+++ mgx_scripts/iterative_growth_and_measures.py
@@ -1,57 +1,106 @@
 ######## New MorphoGraphX session v2.0 r1-63-ga635c756: 2020-08-27 18:16:00
-from os import walk
+import os
+import logging
+
 
 # main directory
-path = 'C:/Users/katha/Desktop/analysis/processing_2_3/mgx/20200307_0715_mgx_meshes_view_files/'
+path = '~/Desktop/202003_0715_analysis'
 
 # get list of files in dir https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
-f = []
+meshes = []
+
+print(os.walk(os.path.join(path, 'meshes')))
+
+# make dirs to save
+
+for (dirpath, dirnames, filenames) in os.walk(os.path.join(path, 'meshes')):
+    meshes.extend(filenames)
+    print(meshes.extend(filenames))
+    break
 
 
+# https://stackoverflow.com/questions/1274405/how-to-create-new-folder
+
+if not os.path.exists(os.path.join(path, 'parents')):
+    os.makedirs(os.path.join(path, 'parents'))
+
+if not os.path.exists(os.path.join(path, 'attributes')):
+    os.makedirs(os.path.join(path, 'attributes'))
+
+
+i = 0
 # open mgx
 # load stack 1, stack 2
-Process.Mesh__System__Load('d1_pAR393xpLH13_mesh_seg.mgxm', 'no', 'no', '0')
-Process.Mesh__System__Load('C:/Users/katha/Desktop/analysis/processing_2_3/mgx/20200307_0715_mgx_meshes_view_files/d2_pAR393xpLH13_mesh.mgxm', 'no', 'no', '1')
-
-# set stack 1 as main
-Process.Stack__System__Set_Current_Stack('Main', '0')
-
-# load special axis heat
-Process.Mesh__Heat_Map__Heat_Map_Load('', '2', '1.0')
-
-# run all measures, <<based on special axis>>
-Process.Mesh__Heat_Map__Analysis__Cell_Analysis_2D('No')
-Process.Mesh__Heat_Map__Operators__Export_Heat_to_Attr_Map('Measure Label Double', 'test', 'Label', 'Label Heat', 'Active Mesh', 'No')
-Process.Mesh__Attributes__Save_to_CSV('')
-
-# 'click' parents in stack 2
-# Process.Mesh__Lineage_Tracking__Parent_Import_from_Attr_Map('Measure Label Int', 'Parents')
+                                #filename, transform, add, stack number (0 indexed)
+Process.Mesh__System__Load(os.path.join(path, 'meshes', meshes[i]), 'no', 'no', '0')
+Process.Mesh__System__Load(os.path.join(path, 'meshes', meshes[i+1]), 'no', 'no', '1')
 
 
-# run growth pipeline
-Process.Mesh__Heat_Map__Analysis__Growth_Analysis_2D('', 'T2', 'T3', 'Yes')
-
-# export attribute map <<based on on axis>>
-Process.Mesh__Heat_Map__Operators__Export_Heat_to_Attr_Map('Measure Label Double', 'test', 'Label', 'Label Heat', 'Active Mesh', 'No')
-Process.Mesh__Attributes__Save_to_CSV('')
-
-# photos of heatmaps
-Process.Mesh__Heat_Map__Heat_Map_Load('', '2', '1.0')
-Process.Misc__System__Snapshot('snap_test', 'false', '0', '0', '1.0', '95')
-
-
-
-
-Process.Stack__System__Set_Current_Stack('Main', '2')
-Process.Stack__System__Set_Current_Stack('Main', '1')
-
-Process.Mesh__Heat_Map__Heat_Map_Save('')
-Process.Mesh__Lineage_Tracking__Save_Parents('', 'Yes')
-Process.Mesh__Lineage_Tracking__Load_Parents('', 'CSV', 'No')
-Process.Misc__System__Snapshot('', 'false', '0', '0', '1.0', '95')
-Process.Misc__System__Snapshot('snap_test', 'false', '0', '0', '1.0', '95')
-
-
-
-
-
+# # todo check that parent file exists, if not save
+#
+# if not os.path.exists(os.join(path, 'parents', meshes[i+1])):
+#     # set stack 1 as main                   store, stack id
+#     Process.Stack__System__Set_Current_Stack('Main', '1')
+#     # maybe do a safety parent save first
+#     #                                           filename, save only existing labels
+#     Process.Mesh__Lineage_Tracking__Save_Parents(os.path.join(path, 'parents', meshes[i+1]), 'Yes')
+#     # save to attr map                          prefix?, attr name
+#     Process.Mesh__Lineage_Tracking__Parent_Export_to_Attr_Map('', 'Yes')
+#
+# #                                           filename, filetype, keep existing parents?
+# Process.Mesh__Lineage_Tracking__Load_Parents(os.path.join(path, 'parents', meshes[i]), 'CSV', 'No')
+# # set stack 1 as main                   store, stack id
+# Process.Stack__System__Set_Current_Stack('Main', '0')
+#
+#
+# # load special axis heat
+# # todo check if there is a special axis to load, then load if so
+# # todo for each heat map
+#
+# # if axis                             filename, column, border size (wall colormaps)
+# Process.Mesh__Heat_Map__Heat_Map_Load('', '2', '1.0')
+#
+# # if no axis
+# # can you open a dialoque
+# # "ok I have marked the axis"
+# # run heatmap
+# # expore to attr map
+# # Process.Mesh__Heat_Map__Operators__Export_Heat_to_Attr_Map('Measure Label Double', 'test', 'Label', 'Label Heat', 'Active Mesh', 'No')
+#
+#
+# # run all measures, <<based on special axis>>
+# #                                           exclude slow processes
+# Process.Mesh__Heat_Map__Analysis__Cell_Analysis_2D('No')
+# #                                      filename
+# Process.Mesh__Attributes__Save_to_CSV('')
+#
+# # 'click' parents in stack 2
+# # Process.Mesh__Lineage_Tracking__Parent_Import_from_Attr_Map('Measure Label Int', 'Parents')
+#
+# # todo set the parents to active, then stack 0
+# # run growth pipeline
+# Process.Mesh__Heat_Map__Analysis__Growth_Analysis_2D('', 'T2', 'T3', 'Yes')
+#
+# # export attribute map <<based on on axis>>
+# Process.Mesh__Heat_Map__Operators__Export_Heat_to_Attr_Map('Measure Label Double', 'test', 'Label', 'Label Heat', 'Active Mesh', 'No')
+# Process.Mesh__Attributes__Save_to_CSV('')
+#
+# # photos of heatmaps
+# Process.Mesh__Heat_Map__Heat_Map_Load('', '2', '1.0')
+# Process.Misc__System__Snapshot('snap_test', 'false', '0', '0', '1.0', '95')
+#
+#
+#
+#
+# Process.Stack__System__Set_Current_Stack('Main', '2')
+# Process.Stack__System__Set_Current_Stack('Main', '1')
+#
+# Process.Mesh__Heat_Map__Heat_Map_Save('')
+#
+# Process.Misc__System__Snapshot('', 'false', '0', '0', '1.0', '95')
+# Process.Misc__System__Snapshot('snap_test', 'false', '0', '0', '1.0', '95')
+#
+#
+#
+#
+#
