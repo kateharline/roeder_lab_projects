71fec869adbc0b707296abfbaddd5bf26d1164b0
kateharline <harhark8@gmail.com>
2020-10-14 19:06:34 -0400

    fix path

--- mgx_scripts/iterative_growth_and_measures.py
+++ mgx_scripts/iterative_growth_and_measures.py
@@ -272,8 +272,9 @@ def do_display(mesh, measures, ranges, attr_dict, main_path):
             os.path.join(main_path, 'attributes', attr_dict['attributes'][i+1]), measures[i], '1.0')
         Process.Mesh__Heat_Map__Heat_Map_Set_Range(ranges[i][0], ranges[i][1])
         # take photos
-        print('Path '+os.path.join(main_path, 'snaps', attr_dict['attributes'][i+1], " ".join(measures[i].split())))
-        Process.Misc__System__Snapshot(os.path.join(main_path, 'snaps', attr_dict['attributes'][i+1], " ".join(measures[i].split())), 'false', '0', '0',
+        snap_path = os.path.join(main_path, 'snaps', attr_dict['attributes'][i+1][:-4]+" ".join(measures[i].split()))
+        print('Path '+snap_path)
+        Process.Misc__System__Snapshot(snap_path, 'false', '0', '0',
                                        '1.0', '95')
 
         # if PDG
