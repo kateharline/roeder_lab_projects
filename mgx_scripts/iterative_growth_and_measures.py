6021a60fd17309508c14af314ceda658b4047a40
kateharline <harhark8@gmail.com>
2021-01-07 15:10:40 -0500

    remove unnecesary prints -- type and save seems to work, loading mesh at proper time under dev

--- mgx_scripts/iterative_growth_and_measures.py
+++ mgx_scripts/iterative_growth_and_measures.py
@@ -178,19 +178,18 @@ def do_distance_measures(meshes, types, path):
         sys.exit('Select cells for '+ types[step % len(types)]+ ' axis then re-run script')
 
     elif (step > 0) and (step < total_steps):
+        print (step)
         # measure distance                                      wall weight, restrict connectivity
         Process.Mesh__Heat_Map__Measures__Location__Cell_Distance('Euclidean', 'No')
         # save as attributes
-        print ('export heat save as '+ types[(step - 1) % len(types)])
         Process.Mesh__Heat_Map__Operators__Export_Heat_to_Attr_Map('Measure Label Double', types[(step - 1) % len(types)] + '_Distance', 'Label',
                                                                'Label Heat', 'Active Mesh', 'No')
         # save the mesh (attributes saved in mesh)
         #                           filename, transform, mesh number
         Process.Mesh__System__Save(os.path.join(path, 'meshes', meshes[(step - 1) // len(types)]), 'no', '0')
-        print('mesh save name '+meshes[(step - 1) // len(types)])
 
 
-        if (step % len(types)):
+        if not(step % len(types)):
             print('open new mesh '+ meshes[(step+1) // len(types)])
             # load new mesh because done all measures
             load_mesh(os.path.join(meshes_path, meshes[(step+1) // len(types)]), 0, 'No')
